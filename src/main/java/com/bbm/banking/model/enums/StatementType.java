fb867f78fe2b1da94cd4247204054b14a8b47229
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-16 22:45:39 +0200

    the enum with the statement types

---
+++ src/main/java/com/bbm/banking/model/enums/StatementType.java
@@ -0,0 +1,11 @@
+package com.bbm.banking.model.enums;
+
+public enum StatementType {
+
+    TRANSFER,
+    DEPOSIT,
+    WITHDRAW,
+    INVOICE_PAYMENT,
+    CREDIT_CARD_PAYMENT,
+    DEBIT_CARD_PAYMENT
+}
