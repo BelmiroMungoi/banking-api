259bf69231547136f0c0db21e4664c19c8df3099
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-18 12:07:36 +0200

    the bank account repository

---
+++ src/main/java/com/bbm/banking/repository/BankAccountRepository.java
@@ -0,0 +1,7 @@
+package com.bbm.banking.repository;
+
+import com.bbm.banking.model.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+}
