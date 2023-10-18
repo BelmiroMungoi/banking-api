eaa9db55d23bd6400b59c52c36df11b7c8c187f7
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-18 23:30:50 +0200

    Adding a new message success for deposit

--- src/main/java/com/bbm/banking/utils/BankUtils.java
+++ src/main/java/com/bbm/banking/utils/BankUtils.java
@@ -3,5 +3,6 @@ package com.bbm.banking.utils;
 public class BankUtils {
 
     public static final String ACCOUNT_CREATED_SUCCESSFULLY = "A conta foi criada com sucesso";
+    public static final String DEPOSIT_CREATED_SUCCESSFULLY = "O depósito foi realizado com sucesso";
 
 }
