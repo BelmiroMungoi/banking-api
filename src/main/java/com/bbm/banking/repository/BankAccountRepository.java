8bf59d72fbfd2032c7938e3eb49d39051be3a059
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-18 23:37:17 +0200

    Adding a method to verify if an account exists by account number

--- src/main/java/com/bbm/banking/repository/BankAccountRepository.java
+++ src/main/java/com/bbm/banking/repository/BankAccountRepository.java
@@ -4,4 +4,6 @@ import com.bbm.banking.model.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
 }
