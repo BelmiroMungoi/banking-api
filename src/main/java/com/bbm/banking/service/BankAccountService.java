9fab885d33dbe8df5a160ca66700cab2b25c9359
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-18 23:41:46 +0200

    Adding a method to deposit and a method to find/get the account by id

--- src/main/java/com/bbm/banking/service/BankAccountService.java
+++ src/main/java/com/bbm/banking/service/BankAccountService.java
@@ -1,13 +1,19 @@
 package com.bbm.banking.service;
 
 import com.bbm.banking.dto.request.AccountRequestDto;
+import com.bbm.banking.dto.request.DepositRequest;
 import com.bbm.banking.dto.response.AccountInfo;
 import com.bbm.banking.dto.response.HttpResponse;
+import com.bbm.banking.model.BankAccount;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface BankAccountService {
 
     HttpResponse createAccount(AccountRequestDto accountRequestDto);
+    HttpResponse deposit(DepositRequest depositRequest);
     List<AccountInfo> findAllAccounts();
+    AccountInfo findAccountById(Long accountId);
+    BankAccount getAccountById(Long accountId);
 }
