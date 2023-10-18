0781899205b73a99a4bdea83c5a526b745cf33c9
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-18 23:40:55 +0200

    Adding the methods to verify if a user already exists in the db

--- src/main/java/com/bbm/banking/repository/UserRepository.java
+++ src/main/java/com/bbm/banking/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.bbm.banking.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
 }
