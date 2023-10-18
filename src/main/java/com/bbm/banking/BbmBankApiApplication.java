b2759265fda3345fdf736e683d6b7466f0777efe
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-18 12:15:51 +0200

    excluding the security auto configuration

--- src/main/java/com/bbm/banking/BbmBankApiApplication.java
+++ src/main/java/com/bbm/banking/BbmBankApiApplication.java
@@ -2,8 +2,9 @@ package com.bbm.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BbmBankApiApplication {
 
 	public static void main(String[] args) {
