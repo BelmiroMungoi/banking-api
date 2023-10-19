65f2d167c3f834aa5327c0685ac3b12e1f609fe1
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-19 12:30:14 +0200

    Adding the methods to transfer and get the account by account number

--- src/main/java/com/bbm/banking/service/BankAccountService.java
+++ src/main/java/com/bbm/banking/service/BankAccountService.java
@@ -2,6 +2,7 @@ package com.bbm.banking.service;
 
 import com.bbm.banking.dto.request.AccountRequestDto;
 import com.bbm.banking.dto.request.DepositRequest;
+import com.bbm.banking.dto.request.TransferRequest;
 import com.bbm.banking.dto.response.AccountInfo;
 import com.bbm.banking.dto.response.HttpResponse;
 import com.bbm.banking.model.BankAccount;
@@ -12,8 +13,10 @@ import java.util.List;
 public interface BankAccountService {
 
     HttpResponse createAccount(AccountRequestDto accountRequestDto);
+    HttpResponse transfer(TransferRequest transferRequest);
     HttpResponse deposit(DepositRequest depositRequest);
     List<AccountInfo> findAllAccounts();
     AccountInfo findAccountById(Long accountId);
     BankAccount getAccountById(Long accountId);
+    BankAccount getAccountByAccountNumber(String accountNumber);
 }
