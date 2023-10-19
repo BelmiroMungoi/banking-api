edc844675b7704dcc0fec49e6bee8f8355a77150
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-19 19:16:10 +0200

    Adding a method to withdraw money add refactoring the parameter name

--- src/main/java/com/bbm/banking/service/BankAccountService.java
+++ src/main/java/com/bbm/banking/service/BankAccountService.java
@@ -1,20 +1,20 @@
 package com.bbm.banking.service;
 
 import com.bbm.banking.dto.request.AccountRequestDto;
-import com.bbm.banking.dto.request.DepositRequest;
+import com.bbm.banking.dto.request.TransactionRequest;
 import com.bbm.banking.dto.request.TransferRequest;
 import com.bbm.banking.dto.response.AccountInfo;
 import com.bbm.banking.dto.response.HttpResponse;
 import com.bbm.banking.model.BankAccount;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface BankAccountService {
 
     HttpResponse createAccount(AccountRequestDto accountRequestDto);
     HttpResponse transfer(TransferRequest transferRequest);
-    HttpResponse deposit(DepositRequest depositRequest);
+    HttpResponse deposit(TransactionRequest transactionRequest);
+    HttpResponse withdraw(TransactionRequest withdrawRequest);
     List<AccountInfo> findAllAccounts();
     AccountInfo findAccountById(Long accountId);
     BankAccount getAccountById(Long accountId);
