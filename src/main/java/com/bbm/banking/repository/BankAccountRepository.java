67ada3a8676587c71c9d69e48111f10279492ad8
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-19 12:29:31 +0200

    adding a method to find the account by account number

--- src/main/java/com/bbm/banking/repository/BankAccountRepository.java
+++ src/main/java/com/bbm/banking/repository/BankAccountRepository.java
@@ -3,7 +3,10 @@ package com.bbm.banking.repository;
 import com.bbm.banking.model.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
 
     boolean existsByAccountNumber(String accountNumber);
+    Optional<BankAccount> findBankAccountByAccountNumber(String accountNumber);
 }
