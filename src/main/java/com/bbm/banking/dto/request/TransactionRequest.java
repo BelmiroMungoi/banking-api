1707b55c264d431eaad75d82a1685caa844241d5
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-19 19:15:09 +0200

    refactoring the class name

--- src/main/java/com/bbm/banking/dto/request/TransactionRequest.java
+++ src/main/java/com/bbm/banking/dto/request/TransactionRequest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
-public class DepositRequest {
+public class TransactionRequest {
 
     private Long accountId;
     private BigDecimal amount;
