753e956d74444e99c19319087dc7c60949e017d4
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-21 20:51:12 +0200

    Adding two methods to find the statements

--- src/main/java/com/bbm/banking/repository/BankStatementRepository.java
+++ src/main/java/com/bbm/banking/repository/BankStatementRepository.java
@@ -3,5 +3,12 @@ package com.bbm.banking.repository;
 import com.bbm.banking.model.BankStatement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BankStatementRepository extends JpaRepository<BankStatement, Long> {
+
+    List<BankStatement> findAllByAccountOwnerId(Long accountId);
+
+    Optional<BankStatement> findByIdAndAccountOwnerId(Long id, Long accountId);
 }
