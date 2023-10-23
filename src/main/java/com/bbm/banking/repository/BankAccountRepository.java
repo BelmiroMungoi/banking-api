91429680e94f0e4785036d1f5109020927968ed1
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-23 19:43:48 +0200

    find an account by id and contacts

--- src/main/java/com/bbm/banking/repository/BankAccountRepository.java
+++ src/main/java/com/bbm/banking/repository/BankAccountRepository.java
@@ -2,12 +2,15 @@ package com.bbm.banking.repository;
 
 import com.bbm.banking.model.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
 
     boolean existsByAccountNumber(String accountNumber);
+
+    BankAccount findByIdAndContacts(Long id, BankAccount contact);
 
     Optional<BankAccount> findBankAccountByAccountNumber(String accountNumber);
 }
