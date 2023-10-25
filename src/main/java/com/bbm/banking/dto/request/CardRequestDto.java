d8177fe958f3c75f821b15fe6f5e101e8bcd3dce
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-25 13:27:01 +0200

    Adding the card request dto

---
+++ src/main/java/com/bbm/banking/dto/request/CardRequestDto.java
@@ -0,0 +1,15 @@
+package com.bbm.banking.dto.request;
+
+import com.bbm.banking.model.BankAccount;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+public class CardRequestDto {
+
+    private BigDecimal amount;
+    private BankAccount account;
+}
