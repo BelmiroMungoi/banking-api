877e142c41d06b64cf762ff6fd3c533c33a11f88
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-25 18:10:53 +0200

    Implementing the method to make a purchase and find by account number
    Implementing the creditPurchase and make a statement for purchase and payInvoice

--- src/main/java/com/bbm/banking/service/impl/CreditCardServiceImpl.java
+++ src/main/java/com/bbm/banking/service/impl/CreditCardServiceImpl.java
@@ -3,11 +3,14 @@ package com.bbm.banking.service.impl;
 import com.bbm.banking.dto.request.CardRequestDto;
 import com.bbm.banking.dto.response.AccountInfo;
 import com.bbm.banking.dto.response.HttpResponse;
+import com.bbm.banking.exception.BadRequestException;
 import com.bbm.banking.exception.EntityNotFoundException;
 import com.bbm.banking.mapper.Mapper;
 import com.bbm.banking.model.BankAccount;
+import com.bbm.banking.model.BankStatement;
 import com.bbm.banking.model.CreditCard;
 import com.bbm.banking.repository.BankAccountRepository;
+import com.bbm.banking.repository.BankStatementRepository;
 import com.bbm.banking.repository.CreditCardRepository;
 import com.bbm.banking.service.BankAccountService;
 import com.bbm.banking.service.CreditCardService;
@@ -19,8 +22,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import static com.bbm.banking.utils.BankUtils.CREDIT_CARD_CREATED_SUCCESSFULLY;
-import static com.bbm.banking.utils.BankUtils.INVOICE_PAID_SUCCESSFULLY;
+import static com.bbm.banking.utils.BankUtils.*;
 
 @Service
 @RequiredArgsConstructor
@@ -29,11 +31,15 @@ public class CreditCardServiceImpl implements CreditCardService {
     private final BankAccountService accountService;
     private final BankAccountRepository accountRepository;
     private final CreditCardRepository creditCardRepository;
+    private final BankStatementRepository statementRepository;
 
     @Override
     public HttpResponse createCreditCard(String accountNumber) {
-        var generateCardNumber = generateCardNumber();
         var savedAccount = accountService.getAccountByAccountNumber(accountNumber);
+        if (creditCardRepository.existsByBankAccount(savedAccount)) {
+            throw new BadRequestException("Essa conta já possui um cartão de crédito!!!");
+        }
+        var generateCardNumber = generateCardNumber();
         CreditCard creditCard = CreditCard.builder()
                 .cardNumber(generateCardNumber)
                 .balance(new BigDecimal("10000.00"))
@@ -41,6 +47,7 @@ public class CreditCardServiceImpl implements CreditCardService {
                 .bankAccount(savedAccount)
                 .build();
         creditCardRepository.save(creditCard);
+
         return httpResponse(HttpStatus.CREATED,
                 CREDIT_CARD_CREATED_SUCCESSFULLY,
                 savedAccount);
@@ -48,19 +55,45 @@ public class CreditCardServiceImpl implements CreditCardService {
 
     @Override
     public HttpResponse payInvoice(CardRequestDto cardRequestDto) {
-        CreditCard creditCard = findByBankAccount(cardRequestDto.getAccount());
+        CreditCard creditCard = findByAccountNumber(cardRequestDto.getAccountNumber());
+        if (creditCard.isAmountLowerThanInvoice(cardRequestDto.getAmount())) {
+            throw new BadRequestException("Falha na transacção. O valor inserido é menor ao da factura!");
+        }
         creditCard.payInvoice(cardRequestDto.getAmount());
+        var statement = statementRepository.save(BankStatement.createInvoicePaymentStatement(cardRequestDto.getAmount().negate(),
+                "Factura Paga", creditCard.getBankAccount()));
+        creditCard.addStatement(statement);
         creditCardRepository.save(creditCard);
         accountRepository.save(creditCard.getBankAccount());
+
         return httpResponse(HttpStatus.OK,
                 INVOICE_PAID_SUCCESSFULLY,
-                cardRequestDto.getAccount());
+                creditCard.getBankAccount());
+    }
+
+    @Override
+    public HttpResponse makeCreditPurchase(CardRequestDto cardRequestDto) {
+        CreditCard creditCard = findByAccountNumber(cardRequestDto.getAccountNumber());
+        creditCard.makeCreditPurchase(cardRequestDto.getAmount());
+        var statement = statementRepository.save(BankStatement.createCreditCardStatement(cardRequestDto.getAmount(),
+                "Compra Realizada", creditCard.getBankAccount()));
+        creditCard.addStatement(statement);
+        creditCardRepository.save(creditCard);
+        return httpResponse(HttpStatus.OK,
+                CREDIT_PURCHASE_SUCCESSFULLY,
+                creditCard.getBankAccount());
     }
 
     @Override
     public CreditCard findByBankAccount(BankAccount account) {
         return creditCardRepository.findByBankAccount(account).orElseThrow(() ->
-                new EntityNotFoundException("Cartão de crédito não existe"));
+                new EntityNotFoundException("Cartão de crédito não existe!!!"));
+    }
+
+    @Override
+    public CreditCard findByAccountNumber(String accountNumber) {
+        return creditCardRepository.findByBankAccountAccountNumber(accountNumber).orElseThrow(() ->
+                new EntityNotFoundException("Essa conta não possui um cartão de crédito!!!"));
     }
 
     private String generateCardNumber() {
