8b9cba11f8545f9e416bd5b18eb3c7c07ec74275
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-25 18:05:26 +0200

    Adding a new success message

--- src/main/java/com/bbm/banking/utils/BankUtils.java
+++ src/main/java/com/bbm/banking/utils/BankUtils.java
@@ -7,6 +7,7 @@ public class BankUtils {
     public static final String TRANSFER_CREATED_SUCCESSFULLY = "A transferência foi realizada com sucesso";
     public static final String WITHDRAW_CREATED_SUCCESSFULLY = "O saque foi realizado com sucesso";
     public static final String CREDIT_CARD_CREATED_SUCCESSFULLY = "O seu cartão foi gerado com sucesso";
-    public static final String INVOICE_PAID_SUCCESSFULLY = "A factura foi paga com sucesso"
+    public static final String INVOICE_PAID_SUCCESSFULLY = "A factura foi paga com sucesso";
+    public static final String CREDIT_PURCHASE_SUCCESSFULLY = "A compra foi realizada com sucesso";
 
 }
