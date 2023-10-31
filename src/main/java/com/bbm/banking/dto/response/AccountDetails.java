336eb6fb8c287c87eec568b6661d782d02d9aeb7
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-31 12:32:03 +0200

    Adding the account details

---
+++ src/main/java/com/bbm/banking/dto/response/AccountDetails.java
@@ -0,0 +1,16 @@
+package com.bbm.banking.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class AccountDetails {
+
+    private Long accountId;
+    private String accountNumber;
+    private String accountName;
+    private BigDecimal newBalance;
+}
