795f7f135d7a26ba5196086419d832efb20fcef4
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-11-01 16:50:00 +0200

    Adding the authentication controller with the endpoint to authenticate the user

---
+++ src/main/java/com/bbm/banking/controller/AuthenticationController.java
@@ -0,0 +1,26 @@
+package com.bbm.banking.controller;
+
+import com.bbm.banking.dto.request.AuthenticationRequest;
+import com.bbm.banking.dto.response.AuthenticationResponse;
+import com.bbm.banking.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+@Tag(name = "Authentication Management")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+}
