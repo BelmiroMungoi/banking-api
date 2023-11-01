f2e47951f4e8b716062d45998e2f1a71147e450d
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-11-01 14:07:50 +0200

    Adding the token entity

---
+++ src/main/java/com/bbm/banking/model/Token.java
@@ -0,0 +1,37 @@
+package com.bbm.banking.model;
+
+import com.bbm.banking.model.enums.TokenType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdDate;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType;
+    private boolean expired;
+    private boolean revoked;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private  User user;
+
+
+}
