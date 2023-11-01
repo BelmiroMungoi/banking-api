c6e5ea748bb893b2984fc2ce52ccd48556091a09
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-11-01 14:08:10 +0200

    Adding a method to find user by username

--- src/main/java/com/bbm/banking/repository/UserRepository.java
+++ src/main/java/com/bbm/banking/repository/UserRepository.java
@@ -3,9 +3,14 @@ package com.bbm.banking.repository;
 import com.bbm.banking.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUsername(String username);
 
     boolean existsByEmail(String email);
 
     boolean existsByPhoneNumber(String phoneNumber);
+
 }
