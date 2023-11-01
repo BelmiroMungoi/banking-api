7a3fb4e04027ac0201a9b11f44e9bac5afa14f59
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-11-01 16:44:10 +0200

    Implementing the the authentication service with the authenticate and save token methods

---
+++ src/main/java/com/bbm/banking/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,53 @@
+package com.bbm.banking.service.impl;
+
+import com.bbm.banking.dto.request.AuthenticationRequest;
+import com.bbm.banking.dto.response.AuthenticationResponse;
+import com.bbm.banking.model.Token;
+import com.bbm.banking.model.User;
+import com.bbm.banking.model.enums.TokenType;
+import com.bbm.banking.repository.TokenRepository;
+import com.bbm.banking.repository.UserRepository;
+import com.bbm.banking.security.JWTService;
+import com.bbm.banking.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findUserByUsername(request.getUsername()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        saveUserToken(user, jwtToken);
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                .build();
+    }
+
+    @Override
+    public void saveUserToken(User user, String token) {
+        var jwtToken = Token.builder()
+                .user(user)
+                .token(token)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(jwtToken);
+    }
+}
