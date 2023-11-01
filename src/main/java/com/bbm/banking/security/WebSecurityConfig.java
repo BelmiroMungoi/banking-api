3abd3697f26c65d58f220c313b73b0a5ac5a7eac
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-11-01 16:45:13 +0200

    Adding the filter, and permit all users to access the swagger ui, auth, and create account

--- src/main/java/com/bbm/banking/security/WebSecurityConfig.java
+++ src/main/java/com/bbm/banking/security/WebSecurityConfig.java
@@ -4,33 +4,42 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
+    private final JWTAuthenticationFilter authenticationFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/auth**")
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(
+                                "/api/v1/account/create",
+                                "/api/v1/auth/**")
                         .permitAll()
-                        .requestMatchers("/api/v1/account/create")
-                        .permitAll()
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**"
+                        ).permitAll()
                         .anyRequest()
                         .authenticated())
-                .sessionManagement(sessionManager -> sessionManager
+                .sessionManagement((sessionManager) -> sessionManager
                         .sessionCreationPolicy(STATELESS))
-                .authenticationProvider(authenticationProvider);
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
